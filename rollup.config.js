cbffca64cca4beb89b346384ef3ae765fddd3e9c
maik <moewes@users.noreply.github.com>
2019-07-05 22:23:15 +0200

    2. Seite

--- rollup.config.js
+++ rollup.config.js
@@ -3,5 +3,5 @@ import createDefaultConfig from '@open-wc/building-rollup/modern-config';
 // if you need to support IE11 use "modern-and-legacy-config" instead.
 // import createDefaultConfig from '@open-wc/building-rollup/modern-and-legacy-config';
 
-export default createDefaultConfig({ input: './index.html' });
+export default createDefaultConfig({ input: './index.html' }, { input: './zweiteSeite.html'});
 
