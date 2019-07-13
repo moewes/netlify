62411041c86c897f96c325a42277ee615df96d1c
maik <moewes@users.noreply.github.com>
2019-07-13 11:29:03 +0200

    background

--- src/my-component.js
+++ src/my-component.js
@@ -6,44 +6,41 @@ class MyComponent extends LitElement {
 
   render() {
     return html`
-    <ui5-shellbar id="shellbar"
-    primary-title="Corporate Portal" 
-    secondary-title="secondary title" 
-    show-product-switch 
-    show-notifications
-    @productSwitchClick="${this.handleClick}"
-    >
-    <ui5-li slot="menuItems">Application 1</ui5-li>
-  	<ui5-li slot="menuItems">Application 2</ui5-li>
-  	<ui5-li slot="menuItems">Application 3</ui5-li>
-  	<ui5-li slot="menuItems">Application 4</ui5-li>
-  	<ui5-li slot="menuItems">Application 5</ui5-li>
+    <ui5-shellbar id="shellbar" primary-title="Corporate Portal" 
+      secondary-title="secondary title" s
+      how-product-switch
+      show-notifications 
+      @productSwitchClick="${this.handleClick}">
+      <ui5-li slot="menuItems">Application 1</ui5-li>
+      <ui5-li slot="menuItems">Application 2</ui5-li>
+      <ui5-li slot="menuItems">Application 3</ui5-li>
+      <ui5-li slot="menuItems">Application 4</ui5-li>
+      <ui5-li slot="menuItems">Application 5</ui5-li>
     </ui5-shellbar>
     <p>Hello world! automatic :-)</p>
     <ui5-button>Hello world!</ui5-button>
-
+    
     <ui5-popover id="popover" placement-type="Bottom">
-	<div class="popover-header">
-		<ui5-title style="padding: 0.25rem 1rem 0rem 1rem">An Kimura</ui5-title>
-	</div>
-
-	<div class="popover-content">
-		<ui5-list separators="None" >
-			<ui5-li icon="sap-icon://sys-find">App Finder</ui5-li>
-			<ui5-li icon="sap-icon://settings" @click="${this.popoverClick}">Settings</ui5-li>
-			<ui5-li icon="sap-icon://edit">Edit Home Page</ui5-li>
-			<ui5-li icon="sap-icon://sys-help">Help</ui5-li>
-			<ui5-li icon="sap-icon://log">Sign out</ui5-li>
-		</ui5-list>
-	</div>
-</ui5-popover>
+      <div class="popover-header">
+        <ui5-title style="padding: 0.25rem 1rem 0rem 1rem">An Kimura</ui5-title>
+      </div>
+      <div class="popover-content">
+        <ui5-list separators="None">
+          <ui5-li icon="sap-icon://sys-find">App Finder</ui5-li>
+          <ui5-li icon="sap-icon://settings" @click="${this.popoverClick}">Settings</ui5-li>
+          <ui5-li icon="sap-icon://edit">Edit Home Page</ui5-li>
+          <ui5-li icon="sap-icon://sys-help">Help</ui5-li>
+          <ui5-li icon="sap-icon://log">Sign out</ui5-li>
+        </ui5-list>
+      </div>
+    </ui5-popover>
     `;
   }
 
   popoverClick(e) {
     console.log("Halo");
     window.dispatchEvent(
-      new CustomEvent('vaadin-router-go', {detail: {pathname: '/fetch'}}));
+      new CustomEvent('vaadin-router-go', { detail: { pathname: '/fetch' } }));
   }
 
   handleClick(e) {
