026af222b691d0c47008e18b705a1b8dcb7c108e
maik <moewes@users.noreply.github.com>
2019-07-22 20:31:31 +0200

    x

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ let _url = null;
 if (typeof ENV == 'undefined') {
   _url = 'http://localhost:8080';
 } else {
-  _url = 'https://ssl-mam-23701.apps.us-west-2.online-starter.openshift.com/people';
+  _url = ENV;
 };
 
 export const URL = _url;
