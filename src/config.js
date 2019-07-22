03a6c7d0d982a580f605012555d1910788f8c29b
maik <moewes@users.noreply.github.com>
2019-07-22 21:17:28 +0200

    x

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
-let _url = ENV;
-if (typeof ENV == 'undefined' && _url == 'undefined') {
+let _url = 'x';
+if (typeof ENV == 'undefined' && _url == 'x') {
   _url = 'http://localhost:8080';
 } else {
   _url = ENV;
